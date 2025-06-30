fa170854ec438777926fcad464e1c9e999718191
Mostafa khaled <mkostafa79@gmail.com>
2025-06-30 18:25:33 +0300

    Time: 19 ms (5.36%), Space: 22.7 MB (5.06%) - LeetHub

--- 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.cpp
@@ -6,37 +6,34 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    unordered_map<int  , vector<pair<int , int>>>map ; 
-    void solve(TreeNode * root , int x , int y )
-    {
-        if(!root) return ; 
-        map[y].push_back({x , root->val}) ; 
+    void solve(TreeNode* root, int x, int y , unordered_map<int, vector<pair<int, int>>>& map) {
+        if (!root)
+            return;
+        map[y].push_back({x, root->val});
+        solve(root->left, x + 1, y - 1 , map);
 
-        solve(root->left ,x+1 ,  y -1) ; 
-
-        solve(root->right ,x+1 ,  y+1) ; 
+        solve(root->right, x + 1, y + 1 , map);
     }
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-         vector<vector<int>> ans ;
-         solve(root , 0 ,0) ;  
-        for(int i =-1000 ;i<=1000 ; i++)
-        {
-            if(map[i].size())
-            {
-                sort(map[i].begin() , map[i].end());
-                vector<int> t(map[i].size()) ; 
-                for(int j =0 ; j<map[i].size() ; j++)
-                {
-                    t[j]= map[i][j].second ; 
+        vector<vector<int>> ans;
+        unordered_map<int, vector<pair<int, int>>> map;
+        solve(root, 0, 0 , map);
+        for (int i = -1000; i <= 1000; i++) {
+            if (map[i].size()) {
+                sort(map[i].begin(), map[i].end());
+                vector<int> t(map[i].size());
+                for (int j = 0; j < map[i].size(); j++) {
+                    t[j] = map[i][j].second;
                 }
                 ans.push_back(t);
             }
         }
-        return ans ; 
+        return ans;
     }
 };
