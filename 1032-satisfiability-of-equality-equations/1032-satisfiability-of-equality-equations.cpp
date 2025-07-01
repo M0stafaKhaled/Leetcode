0b0bee3fa23d7de1411bcaf6690aa26d505554b7
Mostafa khaled <mkostafa79@gmail.com>
2025-07-01 18:39:17 +0300

    Time: 0 ms (100%), Space: 15.2 MB (20.17%) - LeetHub

--- 1032-satisfiability-of-equality-equations/1032-satisfiability-of-equality-equations.cpp
+++ 1032-satisfiability-of-equality-equations/1032-satisfiability-of-equality-equations.cpp
@@ -3,12 +3,11 @@ public:
     class DSU {
     public:
         vector<int> pr;
-        vector<int> ex;
+
         vector<int> size;
         DSU(int n) {
             pr.resize(n);
             size = vector<int>(n, 1);
-            ex = vector<int>(n, -1);
             for (int i = 0; i < n; i++)
                 pr[i] = i;
         }
@@ -17,7 +16,7 @@ public:
             if (x == pr[x])
                 return x;
 
-            return find(pr[x]);
+            return pr[x] = find(pr[x]);
         }
 
         void con(int x, int y) {
@@ -32,33 +31,13 @@ public:
             pr[py] = px;
             size[px] += size[py];
         }
-        bool check(int x, int y) {
-            int px = find(x);
-            int py = find(y);
-            int a = ex[px];
-            int b = ex[py];
-            if (a == -1 && b == -1) {
-                cout << a << " " << b << endl;
-                return true;
-            }
-            if (a != -1) {
-                if (py == a)
-                    return 0;
-            }
-            if (b != -1) {
-                if (px == b)
-                    return 0;
-            }
 
-            return 1;
-        }
         bool cut(int x, int y) {
             int px = find(x);
             int py = find(y);
             if (px == py)
                 return false;
-            ex[px] = py;
-            ex[py] = px;
+
             return 1;
         }
     };
@@ -72,8 +51,7 @@ public:
             if (equations[i][1] == '!') {
                 pr.push_back({equations[i][0], equations[i][3]});
             } else {
-                if (!dsu.check(a, b))
-                    return false;
+
                 dsu.con(a, b);
             }
         }
