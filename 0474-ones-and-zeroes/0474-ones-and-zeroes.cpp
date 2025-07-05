43c9fd91b66cce67e2cde485e5861d5d4040525b
Mostafa khaled <mkostafa79@gmail.com>
2025-07-05 12:26:00 +0300

    Time: 157 ms (47.52%), Space: 39 MB (57.84%) - LeetHub

---
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int db[601][101][101];
+    int solve(vector<string>& strs, int m, int n, int idx) {
+        if (m < 0 || n < 0)
+            return -1e9;
+        if (idx == strs.size())
+            return 0;
+        //  if(m==0 && n==0) return 1 ;
+
+        if (db[idx][m][n] != -1)
+            return db[idx][m][n];
+        int d = 0;
+        int r = 0;
+        int ans = solve(strs, m, n, idx + 1);
+        for (int i = 0; i < strs[idx].size(); i++) {
+            if (strs[idx][i] == '1')
+                d++;
+            if (strs[idx][i] == '0')
+                r++;
+        }
+        if (d <= n && r <= m)
+            ans = max(ans, solve(strs, m - r, n - d, idx + 1) + 1);
+        return db[idx][m][n] = ans;
+    }
+    int findMaxForm(vector<string>& strs, int m, int n) {
+        fill(&db[0][0][0], &db[0][0][0] + 601 * 101 * 101, -1);
+
+        return solve(strs, m, n, 0);
+    }
+};
