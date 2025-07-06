3c3e64e393ac69fefea10f0d2d515d52359e66cc
Mostafa khaled <mkostafa79@gmail.com>
2025-07-07 01:25:08 +0300

    Time: 2 ms (15.65%), Space: 10.5 MB (5.52%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> ans ; 
+    void solve(int idx , int n , vector<int>& cur)
+    {
+        if(idx > n) return ; 
+        vector<int> next(idx);
+        next[0] =1 ; 
+        for(int i = 1 ; i<idx-1 ; i++)
+        {
+            next[i] = cur[i-1] + cur[i] ; 
+        }
+        next[idx-1] = 1 ; 
+        ans.push_back(next) ;
+        solve(idx+1 , n , next) ; 
+    } 
+    vector<vector<int>> generate(int numRows) {
+         ans.push_back({1}) ; 
+        if(numRows ==1) return  ans ; 
+        
+        vector<int> cur(2 , 1) ; 
+        solve(2 , numRows , cur) ; 
+
+        return ans ;  
+         
+    }
+};
