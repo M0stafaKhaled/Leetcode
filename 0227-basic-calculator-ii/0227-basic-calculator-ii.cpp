04cb0628694c932c024c6a2ada77434a43120e1e
Mostafa khaled <mkostafa79@gmail.com>
2025-07-06 14:53:44 +0300

    Time: 958 ms (5.07%), Space: 16.6 MB (13.97%) - LeetHub

---
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    int calculate(string cur) {
+
+        stack<int> st;
+        string s = "";
+        for (int i = 0; i < cur.size(); i++) {
+            if (cur[i] != ' ')
+                s += cur[i];
+        }
+        char x = '+';
+        for (int i = 0; i < s.size(); i++) {
+            int num = 0;
+            while (i < s.size() && s[i] - '0' < 10 && s[i] - '0' >= 0) {
+                int dd = s[i] - '0';
+
+                num = num * 10;
+                num += s[i] - '0';
+                i++;
+            }
+            cout << num  << " "  << x << endl;
+            if (x == '+') {
+                st.push(num);
+            } else if (x == '-') {
+                st.push(-num);
+            } else if (x == '/') {
+                int top = st.top();
+                st.pop();
+                st.push(top / num);
+            } else {
+                int top = st.top();
+                st.pop();
+                st.push(top * num);
+            }
+            if (i  >= s.size())
+                break;
+           
+            x = s[i];
+            
+        }
+
+        int ans = 0;
+        while (st.size()) {
+            int top = st.top();
+            st.pop();
+
+            ans += top;
+        }
+        return ans;
+    }
+};
