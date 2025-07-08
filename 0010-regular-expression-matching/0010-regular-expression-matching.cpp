b0148575f72a69190b9345d3a38a5772e30cad2e
Mostafa khaled <mkostafa79@gmail.com>
2025-07-08 17:52:09 +0300

    Time: 474 ms (9.07%), Space: 8.1 MB (80.83%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
     bool solve(string& s, string& p, int i, int j) {
-        
+
         if (j == p.size())
             return i == s.size();
-        if(i>s.size()) return 0;
+        if (i > s.size())
+            return 0;
         if (j + 1 < p.size() && p[j + 1] == '*') {
-
-            return solve(s, p, i, j + 2) ||
-                    ((p[j] == s[i] || p[j] == '.')) && solve(s, p, i + 1, j)  ;
+            bool check = solve(s, p, i, j + 2);
+            if (((p[j] == s[i] || p[j] == '.')))
+                check |= solve(s, p, i + 1, j);
+            return check;
 
         } else {
-            return   ( (p[j] == '.' || p[j] == s[i])) && solve(s, p, i + 1, j + 1) ;
+            if (((p[j] == '.' || p[j] == s[i])))
+                return solve(s, p, i + 1, j + 1);
+            return false;
         }
-
-        
     }
     bool isMatch(string s, string p) { return solve(s, p, 0, 0); }
 };
