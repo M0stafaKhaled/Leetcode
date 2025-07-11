668d1bea8e42ab2d362dd4947175e8e123bdeada
Mostafa khaled <mkostafa79@gmail.com>
2025-07-12 02:51:44 +0300

    Time: 0 ms (100%), Space: 32.6 MB (27.64%) - LeetHub

--- 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.cpp
+++ 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.cpp
@@ -5,28 +5,24 @@ public:
 
         sort(copy.begin(), copy.end());
 
-
         for (int i = 0; i < nums.size(); i++) {
             // __builtin_popcount(nums[i])
             int h = __builtin_popcount(nums[i]);
-            int cur = i ;
-            bool check = false;
-            while(i+1 <nums.size() &&__builtin_popcount(nums[i+1]) == h)
-            {
+            int cur = i;
+
+            while (i + 1 < nums.size() &&
+                   __builtin_popcount(nums[i + 1]) == h) {
                 i++;
-                check = true ;
             }
-            cout<< cur << " " << i << endl ; 
-            sort(nums.begin()+cur , nums.begin()+(i+1) ) ; 
 
+            sort(nums.begin() + cur, nums.begin() + (i + 1));
         }
 
-        for(int i =0 ; i<nums.size() ; i++)
-        {   cout<<nums[i] << " " << copy[i] <<endl ; 
-            if(nums[i] != copy[i]) return 0 ; 
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != copy[i])
+                return 0;
         }
 
-
-        return 1 ; 
+        return 1;
     }
 };
