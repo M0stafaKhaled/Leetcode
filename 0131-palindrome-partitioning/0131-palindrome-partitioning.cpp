e827f4cc04413af1ab3dedcb0629587968bfd8e1
Mostafa khaled <mkostafa79@gmail.com>
2025-07-12 14:28:30 +0300

    Time: 53 ms (53.23%), Space: 79.9 MB (24.97%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -6,31 +6,33 @@ public:
         while (l < r) {
             if (s[l] != s[r])
                 return 0;
-                l++; r--;
+            l++;
+            r--;
         }
 
         return 1;
     }
-    void solve(string& s, int l, int r) {
+    void solve(string& s, int l) {
         if (l == s.size()) {
             ans.push_back(cur);
             return;
         }
-       if(l>s.size()) return ;
+        if (l > s.size())
+            return;
 
         string d = "";
         for (int i = l; i < s.size(); i++) {
             d += s[i];
             if (is_valid(s, l, i)) {
                 cur.push_back(d);
-                solve(s, i+1,r);
+                solve(s, i + 1);
                 cur.pop_back();
             }
         }
     }
     vector<vector<string>> partition(string s) {
 
-        solve(s, 0, 0);
+        solve(s, 0);
 
         return ans;
     }
