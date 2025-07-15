1ed7209e316e4a9ab563c3ceaaf3f70f011ad5af
Mostafa khaled <mkostafa79@gmail.com>
2025-07-15 21:35:42 +0300

    Time: 154 ms (5.03%), Space: 7.8 MB (96.62%) - LeetHub

---
+++ 1160-letter-tile-possibilities/1160-letter-tile-possibilities.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+  int mask =0 ; 
+   int solve(string &s){
+        int ret = 1;
+     
+        for(int i = 0 ; i < s.size();i++){
+            int prev = i -1 ; 
+            if(((mask>>i)&1) || (i  && s[i] == s[i-1] && !((mask>>(prev))&1)  ))continue;
+            mask|= (1<<i) ; 
+             cout<<mask<<endl ; 
+            ret+=solve(s);
+            mask^= ( 1<<i ); 
+        }
+        return ret;
+    }
+    int numTilePossibilities(string tiles) {
+        sort(tiles.begin(),tiles.end());
+        return solve(tiles)-1;
+    }
+};
