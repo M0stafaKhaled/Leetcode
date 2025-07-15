cde386384f673f8c637a599452833d85cec8f6e9
Mostafa khaled <mkostafa79@gmail.com>
2025-07-15 21:36:02 +0300

    Time: 3 ms (74.43%), Space: 7.9 MB (88.01%) - LeetHub

--- 1160-letter-tile-possibilities/1160-letter-tile-possibilities.cpp
+++ 1160-letter-tile-possibilities/1160-letter-tile-possibilities.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
-  int mask =0 ; 
-   int solve(string &s){
+    int mask = 0;
+    int solve(string& s) {
         int ret = 1;
-     
-        for(int i = 0 ; i < s.size();i++){
-            int prev = i -1 ; 
-            if(((mask>>i)&1) || (i  && s[i] == s[i-1] && !((mask>>(prev))&1)  ))continue;
-            mask|= (1<<i) ; 
-             cout<<mask<<endl ; 
-            ret+=solve(s);
-            mask^= ( 1<<i ); 
+
+        for (int i = 0; i < s.size(); i++) {
+            int prev = i - 1;
+            if (((mask >> i) & 1) ||
+                (i && s[i] == s[i - 1] && !((mask >> (prev)) & 1)))
+                continue;
+            mask |= (1 << i);
+
+            ret += solve(s);
+            mask ^= (1 << i);
         }
         return ret;
     }
     int numTilePossibilities(string tiles) {
-        sort(tiles.begin(),tiles.end());
-        return solve(tiles)-1;
+        sort(tiles.begin(), tiles.end());
+        return solve(tiles) - 1;
     }
 };
