955b5649570cb96ea14c38c26c8d15cd2f44788c
Mostafa khaled <mkostafa79@gmail.com>
2025-07-19 01:42:09 +0300

    Time: 19 ms (80.64%), Space: 12.4 MB (94.44%) - LeetHub

--- 1511-count-number-of-teams/1511-count-number-of-teams.cpp
+++ 1511-count-number-of-teams/1511-count-number-of-teams.cpp
@@ -3,21 +3,30 @@ public:
     int numTeams(vector<int>& rating) {
         int ans = 0;
 
-        for (int i = 0; i < rating.size(); i++) {
-            for (int j = i + 1; j < rating.size(); j++) {
+        for (int j = 1; j < rating.size(); j++) {
 
-                for (int k = j + 1; k < rating.size(); k++) {
-                    int a = rating[i];
-                    int b = rating[j];
-                    int c = rating[k];
-                    if (a > b && b > c)
-                        ans++;
-                    if (c > b && b > a)
-                        ans++;
-                }
+            int cur = rating[j];
+            int left = 0;
+            int right = 0;
+
+            int left2 = 0;
+            int right2 = 0;
+            for (int i = j - 1; i >= 0; i--) {
+                if (rating[i] < cur)
+                    left++;
+                if (rating[i] > cur)
+                    left2++;
             }
-        }
+            for (int i = j + 1; i < rating.size(); i++) {
+                if (rating[i] > cur)
+                    right++;
+                if (rating[i] < cur)
+                    right2++;
+            }
 
+            ans += (right * left);
+            ans += (right2 * left2);
+        }
         return ans;
     }
 };
