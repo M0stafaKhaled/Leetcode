099699e24127f1096b57e94057b84c7ed466123d
Mostafa khaled <mkostafa79@gmail.com>
2025-07-27 22:53:10 +0300

    Time: 162 ms (42.42%), Space: 14.4 MB (51.87%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int db[2501] ;
+    int solve(vector<int>& nums , int i)
+    {
+        if(i==nums.size()) return 0 ; 
+        if(db[i] != -1) return db[i] ; 
+       // int ans = solve(nums , i+1) ; 
+        int cur =0 ; 
+        for(int j = i+1 ; j<nums.size() ; j++)
+        {
+            if(nums[j] > nums[i])
+            {
+                cur = max(solve(nums , j)+1 , cur);
+            }
+        }
+
+        return db[i] = cur ;
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        memset(db , -1 , sizeof db) ; 
+
+
+        int ans =0 ;
+        for(int i =0 ; i<nums.size() ; i++)
+        {
+            int cur = solve(nums, i)+1;
+            cout<<cur<<endl ; 
+            ans = max(ans ,  cur);
+        }
+        return  ans;
+    }
+};
