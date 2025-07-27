cf2ce0cb111194c44a0b4f1ca09e6f76f98d7aa3
Mostafa khaled <mkostafa79@gmail.com>
2025-07-27 05:45:11 +0300

    Time: 3 ms (6.78%), Space: 12.2 MB (50.25%) - LeetHub

---
+++ 2316-count-hills-and-valleys-in-an-array/2316-count-hills-and-valleys-in-an-array.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int countHillValley(vector<int>& nums) {
+        unordered_map<int, int> map;
+        int ans = 0;
+        for (int i = 1; i < nums.size() -1; i++) {
+            int count = 1;
+            int cur = nums[i];
+            int first = i - 1;
+            int last = i + 1;
+
+            while (i + 1 < nums.size() && cur == nums[i + 1])
+              {
+                cout<<i <<" " << count <<endl ; 
+                  i++;
+              }
+            if (i + 1 == nums.size())
+                return ans;
+            // hill
+
+            if (cur > nums[first] && cur > nums[i + 1]) {
+                ans += count;
+            }
+
+            if (cur < nums[first] && cur < nums[i + 1])
+                ans += count;
+        }
+        return ans;
+    }
+};
