a46ca327160f2574e567576a9e97b73c41b328de
Mostafa khaled <mkostafa79@gmail.com>
2025-07-28 18:17:27 +0300

    Time: 0 ms (100%), Space: 10.4 MB (71.8%) - LeetHub

--- 0909-stone-game/0909-stone-game.cpp
+++ 0909-stone-game/0909-stone-game.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-    int db[500][500][2] ; 
+    int db[500][2] ; 
     int solve(vector<int>& piles, int i, int j, bool player) {
         if (i > j)
             return 0;
-        if(db[i][j][player] != -1) return db[i][j][player] ; 
+       if(db[i][player] != -1) return db[i][player] ; 
         int v1 = player ? piles[i] : -piles[i];
         int v2 = player ? piles[j] : -piles[j];
         int ans;
@@ -16,10 +16,10 @@ public:
                       solve(piles, i, j - 1, !player) + v2);
         }
 
-        return db[i][j][player] = ans;
+        return db[i][player] = ans;
     }
     bool stoneGame(vector<int>& piles) {
         memset(db , -1 , sizeof db) ; 
-        return solve(piles, 0, piles.size() - 1, 1) >= 0;
+        return 1;
     }
 };
