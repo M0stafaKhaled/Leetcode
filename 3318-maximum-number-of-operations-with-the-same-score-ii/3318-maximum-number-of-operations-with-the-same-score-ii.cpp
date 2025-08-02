b7d3a7023aaa00114e9c169391908e61da717947
Mostafa khaled <mkostafa79@gmail.com>
2025-08-02 22:35:35 +0300

    Time: 389 ms (29.54%), Space: 41 MB (98.31%) - LeetHub

--- 3318-maximum-number-of-operations-with-the-same-score-ii/3318-maximum-number-of-operations-with-the-same-score-ii.cpp
+++ 3318-maximum-number-of-operations-with-the-same-score-ii/3318-maximum-number-of-operations-with-the-same-score-ii.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
-    int db[2000][2000] ; 
+    int db[2000][2000];
     int solve(vector<int>& nums, int i, int j, int t) {
-        if (j - i <=0)
+        if (j - i <= 0)
             return 0;
 
-        if(db[i][j]!= -1) return db[i][j] ; 
+        if (db[i][j] != -1)
+            return db[i][j];
         int ans = 0;
         if (i + 2 <= j) {
             if (nums[i] + nums[i + 1] == t)
@@ -17,25 +18,21 @@ public:
                 ans = max(solve(nums, i, j - 2, t) + 1, ans);
         }
         if (i + 1 <= j) {
-            if (t == nums[i] + nums[j])
-             {
-                
-                   ans = max(ans, solve(nums, i + 1, j - 1, t) + 1);
-             }
+            if (t == nums[i] + nums[j]) {
+
+                ans = max(ans, solve(nums, i + 1, j - 1, t) + 1);
+            }
         }
-        
 
         return db[i][j] = ans;
     }
     int maxOperations(vector<int>& nums) {
-        memset(db , -1 , sizeof db) ;
+        memset(db, -1, sizeof db);
         int ans = 0;
         int i = 0;
         int j = nums.size() - 1;
         if (i + 2 < nums.size()) {
-            ans =
-                max(ans,
-                    solve(nums, 2, j, nums[i] + nums[i + 1]) + 1);
+            ans = max(ans, solve(nums, 2, j, nums[i] + nums[i + 1]) + 1);
         }
         if (j - 2 >= 0) {
 
