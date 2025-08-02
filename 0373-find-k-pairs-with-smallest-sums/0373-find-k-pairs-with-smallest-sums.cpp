75e1b0510e47737ffc6f7db92d387e69bc6cbc93
Mostafa khaled <mkostafa79@gmail.com>
2025-08-02 14:56:07 +0300

    Time: 43 ms (78.53%), Space: 135.4 MB (77.45%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
@@ -6,7 +6,7 @@ public:
         if (nums1.empty() || nums2.empty() || k == 0)
             return ans;
 
-        using T = vector<int>; 
+        using T = array<int, 3>;
         priority_queue<T, vector<T>, greater<T>> pq;
 
         for (int i = 0; i < nums1.size() && i < k; ++i) {
