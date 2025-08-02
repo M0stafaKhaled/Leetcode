b9b30468d08b9bca0f33da397263f10574a11242
Mostafa khaled <mkostafa79@gmail.com>
2025-08-02 15:17:49 +0300

    Time: 11 ms (25.6%), Space: 18.4 MB (36.24%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
-
-             using T = array<int, 3>;
+        using T = array<int, 3>;
         priority_queue<T, vector<T>, greater<T>> pq;
 
+        for (int i = 0; i < matrix.size(); i++) {
+            pq.push({matrix[i][0], i, 0});
+        }
+        int ans = 0;
+        while (k > 1 && pq.size()) {
+            auto [v, i, j] = pq.top();
+            pq.pop();
+            j++;
 
-            for(int i =0 ; i<matrix.size() ; i++)
-            {
-                pq.push({matrix[i][0]   , i  , 0}) ;
+            if (j < matrix.size()) {
+                pq.push({matrix[i][j], i, j});
             }
-            int ans =0 ; 
-            while(k>1 && pq.size())
-            {
-                auto [v , i , j] = pq.top() ;
-                pq.pop() ; 
-                j++;
-                cout<<v<<" ";
-                if(j<matrix.size())
-                {   cout<<matrix[i][j] << " " ;
-                    pq.push({matrix[i][j] , i , j});
-                }
-                cout<<endl ; 
-                k--;
-            }
-            return pq.top()[0] ; 
+            k--;
+        }
+        return pq.top()[0];
     }
 };
