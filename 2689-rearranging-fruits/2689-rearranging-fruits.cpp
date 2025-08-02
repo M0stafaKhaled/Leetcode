59c3f8f0688274c30d1ea709f6ab4e5da9a95729
Mostafa khaled <mkostafa79@gmail.com>
2025-08-02 13:35:07 +0300

    Time: 2095 ms (5.52%), Space: 103.6 MB (21.38%) - LeetHub

---
+++ 2689-rearranging-fruits/2689-rearranging-fruits.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    long long minCost(vector<int>& basket1, vector<int>& basket2) {
+        int valid = 0;
+        int minf = INT_MAX;
+        sort(basket1.begin(), basket1.end());
+        sort(basket2.begin(), basket2.end());
+        unordered_map<int, int> map1, map2;
+        vector<int> cur;
+        for (int i = 0; i < basket2.size(); i++) {
+            minf = min({
+
+                minf, basket2[i], basket1[i]});
+            map1[basket1[i]]++;
+            map2[basket2[i]]++;
+            cout << basket1[i] << " " << basket2[i] << " " << endl;
+        }
+        long long ans = 0;
+        for (auto& e : map1) {
+            if ((map1[e.first] + map2[e.first]) % 2 == 1)
+                return -1;
+
+            if (map1[e.first] > map2[e.first]) {
+
+                int d = (map1[e.first] - map2[e.first]) / 2;
+                while (d--) {
+                    cur.push_back(e.first);
+                }
+            }
+        }
+
+        for (auto& e : map2) {
+            if (map1[e.first] < map2[e.first]) {
+
+                int d = (map2[e.first] - map1[e.first]) / 2;
+                while (d--) {
+                    cur.push_back(e.first);
+                }
+            }
+        }
+        priority_queue<long long, vector<long long>, greater<long long>> pq;
+
+        for (int i = 0; i < cur.size(); i++) {
+
+            pq.push(cur[i]);
+            cout << cur[i] << " ";
+        }
+        int rf = cur.size() / 2;
+       
+        while (rf--) {
+            long long vb = 1L * 2 * minf;
+            ans += min(pq.top(), vb);
+            pq.pop();
+        }
+
+        return ans;
+    }
+};
