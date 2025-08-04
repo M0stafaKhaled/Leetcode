95d789145740c1caef1c8f8a2c2d7684f3814336
Mostafa khaled <mkostafa79@gmail.com>
2025-08-04 21:58:02 +0300

    Time: 10 ms (57%), Space: 29.1 MB (19.97%) - LeetHub

--- 2001-jump-game-vii/2001-jump-game-vii.cpp
+++ 2001-jump-game-vii/2001-jump-game-vii.cpp
@@ -3,13 +3,10 @@ public:
     bool canReach(string s, int minJump, int maxJump) {
 
         vector<int> db(s.size() + 11);
-
-        cout << s.size() << endl;
         int n = s.size() - 1;
         if (s[n] == '1')
             return 0;
 
-       
         db[0] = 0;
         for (int i = 0; i < s.size(); i++) {
             if (s[i] != '0')
@@ -24,18 +21,15 @@ public:
         for (int i = 1; i <= n; i++) {
             db[i] += db[i - 1];
         }
-        for (int i = 0; i <= n; i++) {
-            cout << db[i] << " ";
-        }
-     int max_f = maxJump;
-        int min_f = 0;
+       
+        int max_f = maxJump;
+
         for (int i = 1; i < s.size(); i++) {
             if (s[i] != '0' || db[i] == 0)
                 continue;
-            if (i > max_f)
-             {  cout<<"test"<<endl;
-                   return 0;
-             }
+            if (i > max_f) {
+                return 0;
+            }
 
             int st = i + minJump;
             int end = min(n, i + maxJump);
