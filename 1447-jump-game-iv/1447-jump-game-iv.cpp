41a6f920903c3228213ca89cfb72aec97273dd0e
Mostafa khaled <mkostafa79@gmail.com>
2025-08-04 22:55:52 +0300

    Time: 91 ms (56.22%), Space: 77.5 MB (63.99%) - LeetHub

---
+++ 1447-jump-game-iv/1447-jump-game-iv.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int minJumps(vector<int>& arr) {
+
+        vector<int> de(arr.size(), 1e9);
+        unordered_map<int, vector<int>> map;
+        for (int i = 0; i < arr.size(); i++) {
+            map[arr[i]].push_back(i);
+        }
+        de[0] = 0;
+        queue<int> q;
+        q.push(0);
+
+        while (q.size()) {
+            int top = q.front();
+            q.pop();
+            int v = arr[top];
+            for (int i = map[v].size() -1; i >=0 ; i--) {
+                int cur = map[v][i];
+                if (cur == -1)
+                    continue;
+                map[v][i] = -1;
+
+                if (de[cur] > de[top] + 1) {
+                    de[cur] = de[top] + 1;
+                    q.push(cur);
+                }
+                map[v].pop_back() ; 
+            }
+            if (top - 1 >= 0 && de[top - 1] > de[top] + 1) {
+                de[top - 1] = de[top] + 1;
+                q.push(top - 1);
+            }
+
+            if (top + 1 < arr.size() && de[top + 1] > de[top] + 1) {
+                de[top + 1] = de[top] + 1;
+                q.push(top + 1);
+            }
+        }
+
+        return de.back();
+    }
+};
