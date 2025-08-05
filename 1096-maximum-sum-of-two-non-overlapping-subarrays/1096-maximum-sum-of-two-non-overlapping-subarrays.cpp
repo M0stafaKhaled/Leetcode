e8a84fc0734e924dc8071a8ea0f4330c6d2b89c9
Mostafa khaled <mkostafa79@gmail.com>
2025-08-06 01:30:08 +0300

    Time: 19 ms (7.62%), Space: 12.6 MB (27.7%) - LeetHub

---
+++ 1096-maximum-sum-of-two-non-overlapping-subarrays/1096-maximum-sum-of-two-non-overlapping-subarrays.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    vector<int> db;
+    vector<int> db2;
+    int ans = 0;
+    int solve(vector<int>& nums, int firstLen) {
+        int sum1 = 0;
+        int idx = 0;
+        int f = 0;
+        for (int i = 0; i < firstLen; i++) {
+            sum1 += nums[i];
+        }
+        db[0] = sum1;
+        f = sum1;
+        for (int i = firstLen; i < nums.size(); i++) {
+            sum1 -= nums[i - firstLen];
+            sum1 += nums[i];
+            db[(i - firstLen) + 1] = sum1;
+            if (f < sum1) {
+                f = sum1;
+                idx = (i - firstLen) + 1;
+            }
+            f = max(f, sum1);
+        }
+
+        return f;
+    }
+    int solve2(vector<int> nums, int sz, int sec, vector<int>& nums2) {
+        int sum = 0;
+        for (int i = 0; i < sz; i++) {
+
+            sum += nums[i];
+            nums[i] = -1000;
+        }
+        ans = max(ans, sum + solve(nums, sec));
+        db2[0] = sum;
+        for (int i = sz; i < nums.size(); i++) {
+            nums[i - sz] = nums2[i - sz];
+            sum -= nums[i - sz];
+            sum += nums[i];
+            nums[i] = -1000;
+            db2[(i - sz) + 1] = sum;
+            ans = max(ans, sum + solve(nums, sec));
+        }
+        return ans;
+    }
+    int maxSumTwoNoOverlap(vector<int>& nums, int firstLen, int secondLen) {
+        db = vector<int>(nums.size());
+        db2 = vector<int>(nums.size());
+
+        int m1 = min(firstLen, secondLen);
+        int m2 = max(firstLen, secondLen);
+
+        return solve2(nums, secondLen, firstLen, nums);
+    }
+};
