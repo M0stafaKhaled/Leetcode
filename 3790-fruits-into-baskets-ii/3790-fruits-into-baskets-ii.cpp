ba3b21ef5db8c14273597a56a1e9774dea604722
Mostafa khaled <mkostafa79@gmail.com>
2025-08-05 14:41:06 +0300

    Time: 16 ms (5.19%), Space: 50.2 MB (12.42%) - LeetHub

---
+++ 3790-fruits-into-baskets-ii/3790-fruits-into-baskets-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
+        priority_queue<int> pq;
+        pq.push(baskets[0]);
+        int idx = 1;
+        int ans = baskets.size();
+        unordered_set<int> st;
+        for (int i = 0; i < fruits.size(); i++) {
+
+            int cur = fruits[i];
+            for (int j = 0; j < baskets.size(); j++) {
+                int des = baskets[j];
+                if (st.find(j) != st.end())
+                    continue;
+                if (des >= cur) {
+                    ans--;
+                    st.insert(j);
+                    break;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
