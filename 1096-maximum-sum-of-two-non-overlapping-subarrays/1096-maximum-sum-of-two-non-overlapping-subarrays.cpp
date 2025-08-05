7caf6a947def0e21d0539a683af26016c9c4f8a0
Mostafa khaled <mkostafa79@gmail.com>
2025-08-06 02:17:58 +0300

    Time: 55 ms (5.02%), Space: 12.3 MB (58.55%) - LeetHub

--- 1096-maximum-sum-of-two-non-overlapping-subarrays/1096-maximum-sum-of-two-non-overlapping-subarrays.cpp
+++ 1096-maximum-sum-of-two-non-overlapping-subarrays/1096-maximum-sum-of-two-non-overlapping-subarrays.cpp
@@ -1,24 +1,36 @@
 class Solution {
 public:
-  
+    int db[2001] ; 
     int ans = 0;
     int solve(vector<int>& nums, int firstLen) {
         int sum1 = 0;
         int idx = 0;
         int f = 0;
-        for (int i = 0; i < firstLen; i++) {
+        for (int i = nums.size() -1; i>(nums.size() - firstLen)-1 ; i--) {
+          // cout<<i<<" " ;
+         
             sum1 += nums[i];
+             db[i] = sum1 ; 
         }
-        
+      
+        db[(nums.size() - firstLen)-1]  = sum1 ; 
         f = sum1;
-        for (int i = firstLen; i < nums.size(); i++) {
-            sum1 -= nums[i - firstLen];
+        for (int i =(nums.size() - firstLen)-1; i>=0 ; i--) {
+          //  cout<<i << " " << i + firstLen  <<endl  ; 
+           //  cout<<i + firstLen -1<<" " ; 
+            sum1 -= nums[i + firstLen ];
             sum1 += nums[i];
-          
+          //  cout<<sum1 <<endl ; 
             f = max(f, sum1);
+             db[i] = f ; 
+        }
+    
+        for(int i =0 ; i<nums.size() ; i++)
+        {
+            cout<<db[i] << " " ; 
         }
 
-     
+    
 
         return f;
     }
@@ -27,23 +39,33 @@ public:
         for (int i = 0; i < sz; i++) {
 
             sum += nums[i];
-            nums[i] = -1000;
+            //nums[i] = -1000;
         }
-        ans = max(ans, sum + solve(nums, sec));
+        ans = max(ans, sum + db[sz]);
        
         for (int i = sz; i < nums.size(); i++) {
-            nums[i - sz] = nums2[i - sz];
+           // nums[i - sz] = nums2[i - sz];
             sum -= nums[i - sz];
             sum += nums[i];
-            nums[i] = -1000;
-           
-            ans = max(ans, sum + solve(nums, sec));
+           // nums[i] = -1000;
+            
+            ans = max(ans, sum + db[i+1]);
+
+            
         }
         return ans;
     }
     int maxSumTwoNoOverlap(vector<int>& nums, int firstLen, int secondLen) {
-     
 
-        return solve2(nums, secondLen, firstLen, nums);
+        int m1 = min(firstLen , secondLen);
+        int m2 = max(firstLen , secondLen) ;
+        memset(db , 0 , sizeof db) ; 
+        solve(nums , m1) ; 
+        int ans1 =  solve2(nums, m2, m1, nums);
+         memset(db , 0 , sizeof db) ; 
+         solve(nums , m2) ; 
+         int ans2 =  solve2(nums, m1, m2, nums);
+
+         return max(ans1 , ans2) ; 
     }
 };
