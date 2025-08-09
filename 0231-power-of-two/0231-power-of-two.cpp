2d1cdcaab124cc5c056325e76fc67dabbc6ad0c0
Mostafa khaled <mkostafa79@gmail.com>
2025-08-09 11:44:08 +0300

    Time: 0 ms (100%), Space: 7.9 MB (17.71%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        int count = 0;
+         int count = 0;
         while (n > 0) {
             count += n & 1;  
             n >>= 1;         
         }
         
-        return count == 1 ; 
+        return count == 1 ;
     }
 };
