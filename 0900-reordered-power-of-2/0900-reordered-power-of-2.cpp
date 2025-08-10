32dfa91e7484a598d6b9f6c1a0b6817120118cbf
Mostafa khaled <mkostafa79@gmail.com>
2025-08-10 15:01:00 +0300

    Time: 0 ms (100%), Space: 9.1 MB (51.03%) - LeetHub

--- 0900-reordered-power-of-2/0900-reordered-power-of-2.cpp
+++ 0900-reordered-power-of-2/0900-reordered-power-of-2.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
-    
     bool reorderedPowerOf2(int n) {
-        unordered_set<string> st;
+        string cur1 = to_string(n);
+        sort(cur1.begin(), cur1.end());
         for (int i = 0; i < 31; i++) {
             int power = 1 << i;
             string cur = to_string(power);
             sort(cur.begin(), cur.end());
-            st.insert(cur);
+            if (cur == cur1)
+                return 1;
         }
-        string cur = to_string(n);
-        sort(cur.begin(), cur.end());
 
-        return st.find(cur) != st.end();
+        return 0;
     }
 };
