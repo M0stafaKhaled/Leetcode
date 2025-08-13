9d775f1db7b2df5fe4e7dfa3bfafda4e78cf31e5
Mostafa khaled <mkostafa79@gmail.com>
2025-08-13 17:09:21 +0300

    Time: 347 ms (44.05%), Space: 0B (100%) - LeetHub

--- 2127-employees-whose-manager-left-the-company/2127-employees-whose-manager-left-the-company.sql
+++ 2127-employees-whose-manager-left-the-company/2127-employees-whose-manager-left-the-company.sql
@@ -4,6 +4,6 @@ from employees as e1
 
 where salary <30000
 and manager_id is not null 
-and not EXISTS
-(select  employee_id  from  employees as e2 where e1.manager_id  = e2.employee_id)
+and manager_id  not in
+(select  employee_id  from  employees )
 order by employee_id 
