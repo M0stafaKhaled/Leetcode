e685ae41e920e4867dae5110acb217e31b55ff01
Mostafa khaled <mkostafa79@gmail.com>
2025-08-16 00:21:00 +0300

    Time: 959 ms (5.02%), Space: 31.3 MB (17.92%) - LeetHub

---
+++ 0971-shortest-bridge/0971-shortest-bridge.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+    int shortestBridge(vector<vector<int>>& grid) {
+        queue<pair<int, int>> q;
+        vector<vector<int>> costs(grid.size(),
+                                  vector<int>(grid[0].size(), 1e9));
+
+        for (int i = 0; i < grid.size(); i++) {
+            bool f = false;
+            for (int j = 0; j < grid[i].size(); j++) {
+
+                if (grid[i][j] == 1) {
+                    costs[i][j] = 0;
+                    q.push({i, j});
+                    f = true;
+                    break;
+                }
+            }
+            if (f)
+                break;
+        }
+        int dir[5] = {1, 0, -1, 0, 1};
+        while (q.size()) {
+            auto [x, y] = q.front();
+            q.pop();
+            for (int i = 0; i < 4; i++) {
+                int dx = dir[i] + x;
+                int dy = dir[i + 1] + y;
+                if (dx < 0 || dx >= grid.size() || dy >= grid[0].size() ||
+                    dy < 0)
+                    continue;
+                if (grid[dx][dy] == 1) {
+                    // cout << costs[x][y] << " " << costs[dx][dy] << " ";
+                    // cout << dx << " " << dy << endl;
+                }
+                int co = grid[dx][dy] == 0;
+                if (costs[dx][dy] > costs[x][y] + co) {
+                    //  cout<<dx <<" " << dy <<endl ;
+                    costs[dx][dy] = costs[x][y] + co;
+                    q.push({dx, dy});
+                }
+            }
+        }
+
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[i].size(); j++) {
+                cout << grid[i][j] << " ";
+            }
+            cout << endl;
+        }
+        cout << endl;
+        int ans = 0;
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[i].size(); j++) {
+                if (grid[i][j] == 1) {
+                    ans = max(ans, costs[i][j]);
+                }
+                cout << costs[i][j] << " ";
+            }
+            cout << endl;
+        }
+
+        return ans;
+    }
+};
