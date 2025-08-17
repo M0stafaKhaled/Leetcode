2dd21d8861bc81ab9c925f3dcb4ea124153638c5
Mostafa khaled <mkostafa79@gmail.com>
2025-08-17 22:01:47 +0300

    Time: 3 ms (49.99%), Space: 25.2 MB (71.35%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,18 +1,42 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-       unordered_set<int> st ; 
-       int sum =0 ; 
-       int n = nums.size();
-       int sh  = (n*(n+1))/2;
-       int num =0 ; 
-        for(int i =0 ; i<nums.size(); i++)
-        {
-           sum+=nums[i] ; 
-           if(st.count(nums[i]))   num = nums[i] ; 
-           st.insert(nums[i]) ; 
+
+        int sum = 0;
+        int n = nums.size();
+        int sh = (n * (n + 1)) / 2;
+        int x = 0;
+        int num = 0;
+        int all = 0;
+        for (int i = 1; i <= n; i++) {
+            all ^= i ^ nums[i - 1];
+        }
+        int diff = all & -all;
+        int a = 0;
+        int b = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if ((nums[i] & diff)) {
+                a ^= nums[i];
+            } else
+                b ^= nums[i];
         }
 
-        return {num , sh-(sum-num)} ; 
+        for (int i = 1; i <=nums.size(); i++) {
+            if ((i & diff)) {
+                a ^= i;
+            } else
+                b ^= i;
+        }
+        bool swaps = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == b) {
+                swaps = true;
+                break;
+            }
+        }
+        if (swaps)
+            swap(a, b);
+        return {a, b};
     }
 };
