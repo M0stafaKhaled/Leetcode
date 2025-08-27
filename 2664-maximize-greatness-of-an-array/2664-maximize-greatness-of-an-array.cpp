2e4ae02ddb6cdad5eb396b4dd44d0d0c21c8d710
Mostafa khaled <mkostafa79@gmail.com>
2025-08-27 17:06:37 +0300

    Time: 25 ms (21.73%), Space: 80.7 MB (92.34%) - LeetHub

--- 2664-maximize-greatness-of-an-array/2664-maximize-greatness-of-an-array.cpp
+++ 2664-maximize-greatness-of-an-array/2664-maximize-greatness-of-an-array.cpp
@@ -5,7 +5,6 @@ public:
         int ans = 0;
         int start = 0;
         for (int i = 0; i < nums.size() - 1; i++) {
-
             if (nums[i + 1] > nums[start]) {
                 ans++;
                 start++;
