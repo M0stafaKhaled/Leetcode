55a74468c90d97704aa56565e04ac8c9c107bed0
Mostafa khaled <mkostafa79@gmail.com>
2025-08-27 17:05:47 +0300

    Time: 14 ms (52.09%), Space: 81 MB (33.21%) - LeetHub

--- 2664-maximize-greatness-of-an-array/2664-maximize-greatness-of-an-array.cpp
+++ 2664-maximize-greatness-of-an-array/2664-maximize-greatness-of-an-array.cpp
@@ -3,11 +3,9 @@ public:
     int maximizeGreatness(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         int ans = 0;
-        unordered_map<int, int> map;
-        map[nums[0]]++;
         int start = 0;
         for (int i = 0; i < nums.size() - 1; i++) {
-            map[nums[i]]++;
+
             if (nums[i + 1] > nums[start]) {
                 ans++;
                 start++;
