8d5dd12c43e6a2c745e53e0fd706f0975a43201a
Mostafa khaled <mkostafa79@gmail.com>
2025-08-29 19:41:56 +0300

    Time: 33 ms (5.06%), Space: 16.9 MB (39.08%) - LeetHub

---
+++ 2116-count-number-of-pairs-with-absolute-difference-k/2116-count-number-of-pairs-with-absolute-difference-k.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int countKDifference(vector<int>& nums, int k) {
+        int ans = 0;
+        unordered_map<int, int> map;
+        for (int i = 0; i < nums.size(); i++) {
+            int x = abs(nums[i] - k);
+            int y = abs(nums[i] + k);
+            if (map.find(x) != map.end()) {
+                cout << x << endl;
+                //  ans++;
+            } else if (map.find(y) != map.end()) {
+                cout << y << endl;
+                // ans++;
+            }
+            map[nums[i]]++;
+        }
+
+        for (auto e : map) {
+            int a = e.first + k;
+            if (map.find(a) != map.end()) {
+                int x = (e.second* map[a]);
+                
+                ans += x;
+            }
+        }
+
+        return ans;
+    }
+};
