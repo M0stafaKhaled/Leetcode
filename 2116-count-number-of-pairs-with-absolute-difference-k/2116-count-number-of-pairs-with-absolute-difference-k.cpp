6c770f9b4152423641ae9b66dc4323eb5a9ef65e
Mostafa khaled <mkostafa79@gmail.com>
2025-08-29 19:42:15 +0300

    Time: 3 ms (58.5%), Space: 17.1 MB (24.17%) - LeetHub

--- 2116-count-number-of-pairs-with-absolute-difference-k/2116-count-number-of-pairs-with-absolute-difference-k.cpp
+++ 2116-count-number-of-pairs-with-absolute-difference-k/2116-count-number-of-pairs-with-absolute-difference-k.cpp
@@ -4,23 +4,15 @@ public:
         int ans = 0;
         unordered_map<int, int> map;
         for (int i = 0; i < nums.size(); i++) {
-            int x = abs(nums[i] - k);
-            int y = abs(nums[i] + k);
-            if (map.find(x) != map.end()) {
-                cout << x << endl;
-                //  ans++;
-            } else if (map.find(y) != map.end()) {
-                cout << y << endl;
-                // ans++;
-            }
+
             map[nums[i]]++;
         }
 
         for (auto e : map) {
             int a = e.first + k;
             if (map.find(a) != map.end()) {
-                int x = (e.second* map[a]);
-                
+                int x = (e.second * map[a]);
+
                 ans += x;
             }
         }
