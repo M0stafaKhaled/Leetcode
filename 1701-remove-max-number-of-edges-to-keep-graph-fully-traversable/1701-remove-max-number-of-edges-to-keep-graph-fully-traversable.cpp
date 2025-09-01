31a4f06b0f252e9c03be9d7380650347fb82be05
Mostafa khaled <mkostafa79@gmail.com>
2025-09-01 21:47:03 +0300

    Time: 1672 ms (5.02%), Space: 144 MB (54.43%) - LeetHub

---
+++ 1701-remove-max-number-of-edges-to-keep-graph-fully-traversable/1701-remove-max-number-of-edges-to-keep-graph-fully-traversable.cpp
@@ -0,0 +1,84 @@
+class Solution {
+public:
+    class Dsu {
+    public:
+        vector<int> arr;
+        Dsu(int n) {
+            arr = vector<int>(n);
+            for (int i = 1; i < n; i++) {
+                arr[i] = i;
+            }
+        }
+
+        int find(int x) {
+            if (arr[x] == x)
+                return x;
+
+            return arr[x] = find(arr[x]);
+        }
+
+        void con(int x, int y) {
+            int dx = find(x);
+            int dy = find(y);
+            if (dx == dy)
+                return;
+            arr[dy] = dx;
+        }
+    };
+    int maxNumEdgesToRemove(int n, vector<vector<int>>& edges) {
+        Dsu dsu1(n + 1);
+        Dsu dsu2(n + 1);
+        int ans = 0;
+        for (int i = 0; i < edges.size(); i++) {
+            int a = edges[i][0];
+            int b = edges[i][1];
+            int c = edges[i][2];
+            if (a == 3) {
+                if (dsu1.find(c) == dsu1.find(b) &&
+                    dsu2.find(c) == dsu2.find(b))
+                    ans++;
+                else {
+                    dsu1.con(c, b);
+                    dsu2.con(c, b);
+                }
+            }
+        }
+
+        for (int i = 0; i < edges.size(); i++) {
+            int a = edges[i][0];
+            int b = edges[i][1];
+            int c = edges[i][2];
+            if (a == 3)
+                continue;
+            cout << a << " " << b << " " << c;
+            if (dsu1.find(c) == dsu1.find(b) && a == 1) {
+                cout << "  herer1";
+                ans++;
+            } else {
+                if (a == 1) {
+                    dsu1.con(c, b);
+                }
+            }
+
+            if (dsu2.find(c) == dsu2.find(b) && a == 2) {
+
+                cout << "  herer2" << " ";
+                ans++;
+            } else {
+                if (a == 2) {
+                    dsu2.con(c, b);
+                }
+            }
+            cout << endl;
+        }
+
+        for (int i = 2; i <= n; i++) {
+            if (dsu1.find(i) != dsu1.find(1))
+                return -1;
+            if (dsu2.find(i) != dsu2.find(1))
+                return -1;
+        }
+
+        return ans;
+    }
+};
