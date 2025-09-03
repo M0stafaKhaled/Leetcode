a70fda04e95d41334b39dce9dc5c18ea757ca7b1
Mostafa khaled <mkostafa79@gmail.com>
2025-09-04 00:22:19 +0300

    Time: 182 ms (5.31%), Space: 95 MB (79.95%) - LeetHub

---
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -0,0 +1,68 @@
+class Solution {
+public:
+    int solve(vector<int>& nums1, vector<int>& nums2, int num) {
+        int start = 0;
+        int end = nums1.size() - 1;
+        int n1 = 0;
+        while (end >= start) {
+            int mid = (end + start) / 2;
+            if (nums1[mid] <= num) {
+                start = mid + 1;
+                n1 = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        start = 0;
+        end = nums2.size() - 1;
+        int n2 = 0;
+
+        while (end >= start) {
+            int mid = (end + start) / 2;
+            if (nums2[mid] <= num) {
+                start = mid + 1;
+                n2 = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return n1 + n2;
+    }
+
+    int solve2(vector<int>& nums1, vector<int>& nums2, int se) {
+        int ans = 0;
+        int start = -1e6;
+        int end = 1e6;
+        while (end >= start) {
+            int mid = (end + start) / 2;
+            cout << mid << endl;
+
+            int d = solve(nums1, nums2, mid);
+
+            if (mid == 2) {
+                cout << "testd  " << d << endl;
+            }
+            // cout << d << " " << endl;
+            if (d == se) {
+            }
+            if (d >= se) {
+                ans = mid;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        double ans = 0;
+        int n = nums1.size() + nums2.size();
+        int se = (n + 1) / 2;
+        int se2 = se + (n % 2 == 0);
+
+        return (solve2(nums1, nums2, se) + solve2(nums1, nums2, se2)) * .5;
+    }
+};
