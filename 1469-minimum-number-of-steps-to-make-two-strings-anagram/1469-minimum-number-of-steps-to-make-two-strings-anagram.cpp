674810e979873e9e30454501860fe7f695b0cae8
Mostafa khaled <mkostafa79@gmail.com>
2025-09-08 23:24:55 +0300

    Time: 13 ms (42.39%), Space: 19.3 MB (88.86%) - LeetHub

--- 1469-minimum-number-of-steps-to-make-two-strings-anagram/1469-minimum-number-of-steps-to-make-two-strings-anagram.cpp
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/1469-minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
+    int m1[26], m2[26];
     int minSteps(string s, string t) {
 
-        unordered_map<char, int> m1, m2;
         int ans = 0;
 
         for (int i = 0; i < s.size(); i++) {
-            m1[s[i]]++;
-            m2[t[i]]++;
+            m1[s[i] - 'a']++;
+            m2[t[i] - 'a']++;
         }
 
-        for (auto e : m1) {
-            if (m2[e.first] < e.second) {
-                ans += e.second - m2[e.first];
+        for (int i = 0; i < 26; i++) {
+            if (m2[i] < m1[i]) {
+                ans += m1[i] - m2[i];
             }
         }
 
