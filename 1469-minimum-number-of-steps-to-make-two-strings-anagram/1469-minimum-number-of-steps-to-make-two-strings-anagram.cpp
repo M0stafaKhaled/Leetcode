03e9356befee1d8e68ea960a494a9208dc7db028
Mostafa khaled <mkostafa79@gmail.com>
2025-09-08 23:23:11 +0300

    Time: 17 ms (36.86%), Space: 19.8 MB (9.74%) - LeetHub

--- 1469-minimum-number-of-steps-to-make-two-strings-anagram/1469-minimum-number-of-steps-to-make-two-strings-anagram.cpp
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/1469-minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -4,16 +4,15 @@ public:
 
         unordered_map<char, int> m1, m2;
         int ans = 0;
-       
+
         for (int i = 0; i < s.size(); i++) {
             m1[s[i]]++;
             m2[t[i]]++;
         }
-      
 
         for (auto e : m1) {
             if (m2[e.first] < e.second) {
-                ans += abs(e.second - m2[e.first]);
+                ans += e.second - m2[e.first];
             }
         }
 
