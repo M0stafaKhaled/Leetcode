d38e0831ebd458450ecf14e9ae5f509e59abe4ad
Mostafa khaled <mkostafa79@gmail.com>
2025-09-12 17:41:00 +0300

    Time: 1 ms (91.79%), Space: 18.9 MB (95.71%) - LeetHub

--- 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.cpp
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.cpp
@@ -3,15 +3,10 @@ public:
     bool doesAliceWin(string s) {
 
         int count = 0;
-        int last = 0;
-        int pre = -1;
-
         for (int i = 0; i < s.size(); i++) {
-            if (s[i] == 'a' || s[i] == 'A' || s[i] == 'e' || s[i] == 'E' ||
-                s[i] == 'i' || s[i] == 'I' || s[i] == 'o' || s[i] == 'O' ||
-                s[i] == 'u' || s[i] == 'U') {
-                pre = last;
-                last = i;
+            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' ||
+                s[i] == 'u') {
+
                 count++;
             }
         }
