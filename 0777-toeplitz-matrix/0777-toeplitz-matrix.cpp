b2d0303bb40ed4bfd0ed8d2066ac3e9084cdee92
Mostafa khaled <mkostafa79@gmail.com>
2025-09-20 02:45:34 +0300

    Time: 0 ms (100%), Space: 21.2 MB (10.14%) - LeetHub

--- 0777-toeplitz-matrix/0777-toeplitz-matrix.cpp
+++ 0777-toeplitz-matrix/0777-toeplitz-matrix.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     bool solve(int i, int j, vector<vector<int>>& matrix) {
         while (i + 1 < matrix.size() && j + 1 < matrix[0].size()) {
-            cout << i + 1 << " " << j + 1 << " " << matrix[i + 1][j + 1]
-                 << endl;
+
             if (matrix[i + 1][j + 1] != matrix[i][j])
                 return 0;
             i++, j++;
@@ -17,18 +16,17 @@ public:
         int j = 0;
 
         for (; j < matrix[0].size(); j++) {
-            cout << i << " " << j << " " << matrix[i][j] << endl;
+
             if (!solve(0, j, matrix))
                 return 0;
-            cout << "test" << endl;
         }
-        cout<<endl << endl ;
-        j =0 ;  
+
+        j = 0;
+        i = 1;
         for (; i < matrix.size(); i++) {
-           cout<< i << " " << j << " " << matrix[i][j] << endl;
+
             if (!solve(i, j, matrix))
                 return 0;
-            cout << "test" << endl;
         }
 
         return 1;
