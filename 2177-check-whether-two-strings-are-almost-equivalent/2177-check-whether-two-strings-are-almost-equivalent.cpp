78bc52f9416d2d1ce91928fa273d4b5b56e40283
Mostafa khaled <mkostafa79@gmail.com>
2025-09-20 02:52:47 +0300

    Time: 0 ms (100%), Space: 8.5 MB (55.27%) - LeetHub

--- 2177-check-whether-two-strings-are-almost-equivalent/2177-check-whether-two-strings-are-almost-equivalent.cpp
+++ 2177-check-whether-two-strings-are-almost-equivalent/2177-check-whether-two-strings-are-almost-equivalent.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int arr[26];
-    int arr2[26];
     bool checkAlmostEquivalent(string word1, string word2) {
 
         for (int i = 0; i < word1.size(); i++) {
             arr[word1[i] - 'a']++;
-            arr2[word2[i] - 'a']++;
+            arr[word2[i] - 'a']--;
         }
 
         for (int i = 0; i < 26; i++) {
-            if (abs(arr[i] - arr2[i]) > 3)
+            if (abs(arr[i]) > 3)
                 return 0;
         }
 
