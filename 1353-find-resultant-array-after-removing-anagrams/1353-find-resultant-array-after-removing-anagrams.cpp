e269ebb0b1c64318bc4a00a323b47b7f84f2c686
Mostafa khaled <mkostafa79@gmail.com>
2025-10-14 00:02:44 +0300

    Time: 8 ms (13.83%), Space: 17.9 MB (24.35%) - LeetHub

---
+++ 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<string> removeAnagrams(vector<string>& words) {
+        unordered_set<string> st;
+        unordered_map<int, bool> map;
+        unordered_map<int, string> map2;
+        vector<string> ans;
+        for (int i = 0; i < words.size(); i++) {
+            if (map2.find(i) != map2.end()) {
+                ans.push_back(map2[i]);
+            } else {
+                ans.push_back(words[i]);
+            }
+            if (!map[i])
+                sort(words[i].begin(), words[i].end());
+
+            while (i + 1 < words.size()) {
+                map2[i + 1] = words[i + 1];
+                map[i + 1] = 1;
+                sort(words[i + 1].begin(), words[i + 1].end());
+                if (words[i] == words[i + 1])
+                    i++;
+                else {
+                    break;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
