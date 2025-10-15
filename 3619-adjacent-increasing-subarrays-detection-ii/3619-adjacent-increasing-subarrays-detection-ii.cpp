336f8b8020fe41e6189374ed45e701ae10bc7e97
Mostafa khaled <mkostafa79@gmail.com>
2025-10-15 23:34:43 +0300

    Time: 234 ms (50.4%), Space: 181.9 MB (40.89%) - LeetHub

--- 3619-adjacent-increasing-subarrays-detection-ii/3619-adjacent-increasing-subarrays-detection-ii.cpp
+++ 3619-adjacent-increasing-subarrays-detection-ii/3619-adjacent-increasing-subarrays-detection-ii.cpp
@@ -1,18 +1,8 @@
 class Solution {
 public:
-    bool solve(const vector<int>& nums, int k) {
-        int n = nums.size();
-        if (n < 2 * k)
-            return false;
+    bool solve(const vector<int>& run, int k) {
+        int n = run.size();
 
-        vector<int> run(n, 1);
-
-        for (int i = n - 2; i >= 0; i--) {
-            if (nums[i + 1] > nums[i])
-                run[i] = run[i + 1] + 1;
-            else
-                run[i] = 1;
-        }
         for (int i = 0; i + 2 * k <= n; i++) {
             if (run[i] >= k && run[i + k] >= k)
                 return true;
@@ -24,10 +14,19 @@ public:
         int start = 0;
         int end = nums.size();
         int ans = 1;
+        int n = nums.size();
+        vector<int> run(n, 1);
+
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i + 1] > nums[i])
+                run[i] = run[i + 1] + 1;
+            else
+                run[i] = 1;
+        }
         while (start <= end) {
             int mid = (start + end) / 2;
             // cout << mid << " " << endl;
-            if (solve(nums, mid)) {
+            if (solve(run, mid)) {
                 ans = max(ans, mid);
                 start = mid + 1;
             } else {
