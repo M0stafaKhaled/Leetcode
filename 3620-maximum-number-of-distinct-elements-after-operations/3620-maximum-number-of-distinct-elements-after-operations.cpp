3068a6742adbad2f35f544b0a7e0fa6913e7c661
Mostafa khaled <mkostafa79@gmail.com>
2025-10-18 17:54:43 +0300

    Time: 983 ms (5.36%), Space: 97.5 MB (98.74%) - LeetHub

--- 3620-maximum-number-of-distinct-elements-after-operations/3620-maximum-number-of-distinct-elements-after-operations.cpp
+++ 3620-maximum-number-of-distinct-elements-after-operations/3620-maximum-number-of-distinct-elements-after-operations.cpp
@@ -2,15 +2,13 @@ class Solution {
 public:
     int maxDistinctElements(vector<int>& nums, int k) {
         int ans = 1;
-
-        unordered_map<long long, int> map;
         sort(nums.begin(), nums.end());
         int start = -k;
         int f = nums[0] + start;
         for (int i = 1; i < nums.size(); i++) {
             long long next = f + 1;
             long long cur = next - nums[i];
-         //   cout << cur << endl;
+            //   cout << cur << endl;
             int j = next;
             bool check = false;
             for (; j <= nums[i] + k; j++) {
