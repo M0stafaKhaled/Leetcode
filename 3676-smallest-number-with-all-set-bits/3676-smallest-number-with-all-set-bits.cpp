52195e295c7996ed6ee14fa32c6af13bc2d2207a
Mostafa khaled <mkostafa79@gmail.com>
2025-10-29 20:16:17 +0300

    Time: 11 ms (2.57%), Space: 8.3 MB (60.8%) - LeetHub

---
+++ 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int smallestNumber(int n) {
+        if (n == 1)
+            return n;
+
+        for (int i = 2; i < 31; i++) {
+            int cur = 1 << i;
+            if (cur - 1 >= n)
+                return cur - 1;
+            cout << cur - 1 << endl;
+        }
+
+        return 1;
+    }
+};
