8e2ac2ca30f2af84c6dc24e61872e7ce1fc52fdc
Mostafa khaled <mkostafa79@gmail.com>
2025-11-25 22:16:56 +0200

    Time: 0 ms (100%), Space: 7.8 MB (80.92%) - LeetHub

--- 1015-smallest-integer-divisible-by-k/1015-smallest-integer-divisible-by-k.cpp
+++ 1015-smallest-integer-divisible-by-k/1015-smallest-integer-divisible-by-k.cpp
@@ -4,21 +4,23 @@ public:
 
         if (k % 2 == 0)
             return -1;
+
+        if (k % 5 == 0)
+            return -1;
         if (k == 1)
             return 1;
 
-        unsigned long long max = 1e18;
         unsigned long long cur = 1;
         int n = 1;
 
-        while (max >= cur && n <=1e5) {
+        while (n <= k) {
             if ((cur) % k == 0)
                 return n;
 
             n++;
             cur = ((cur * 10) + 1) % k;
         }
-        cout << cur << endl;
+     //   cout << cur << endl;
         return -1;
     }
 };
