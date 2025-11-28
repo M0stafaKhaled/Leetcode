bdf14371922a6ce8858e05e8e9da56070655b478
Mostafa khaled <mkostafa79@gmail.com>
2025-11-28 21:06:36 +0200

    Time: 84 ms (53.01%), Space: 184.2 MB (20.69%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,69 +1,42 @@
 class LRUCache {
 public:
-    struct Node {
-        int key;
-        int val;
-        Node* prev;
-        Node* next;
-        Node(int k, int v) : key(k), val(v), prev(nullptr), next(nullptr) {}
-    };
-
-    unordered_map<int, Node*>
-        map;
-
-    Node* head;
-    Node* tail;
-    int cap;
-    void removeNode(Node* node) {
-
-        node->prev->next = node->next;
-        node->next->prev = node->prev;
-    }
-    void addToFront(Node* node) {
-        node->next = head->next;
-        node->prev = head;
-        head->next = node;
-        node->next->prev = node;
-    }
+    priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>,
+                   std::greater<std::pair<int, int>>>
+        pq;
+    int timer = 0;
+    int cap = 0;
+    int st = 1;
+    unordered_map<int, int> map;
+    unordered_map<int, int> counter;
+    unordered_map<int, int> time_map;
+    LRUCache(int capacity) { cap = capacity; }
 
     int get(int key) {
         if (map.find(key) == map.end())
             return -1;
-        Node* node = map[key];
-        moveToFront(node);
-        return node->val;
-    }
 
-    void moveToFront(Node* node) {
-        removeNode(node);
-        addToFront(node);
-    }
-
-    LRUCache(int capacity) {
-        cap = capacity;
-        head = new Node(-1, -1);
-        tail = new Node(-1, -1);
-        head->next = tail;
-        tail->prev = head;
+        counter[key]++;
+        timer++;
+        time_map[timer] = key;
+        return map[key];
     }
 
     void put(int key, int value) {
 
-        if (map.find(key) == map.end()) {
-            if (map.size() == cap) {
-                Node* node = tail->prev;
-
-                map.erase(node->key);
-                removeNode(node);
+        map[key] = value;
+        counter[key]++;
+        timer++;
+        // pq.push({timer, key});
+        while (map.size() > cap) {
+            int k = time_map[st];
+            st++;
+            counter[k]--;
+            if (counter[k] <= 0) {
+                map.erase(k);
+                counter.erase(k);
             }
-            Node* cur = new Node(key, value);
-            addToFront(cur);
-            map[key] = cur;
-        } else {
-             moveToFront(map[key]);
-            map[key]->val = value;
-           
         }
+        time_map[timer] = key;
     }
 };
 
