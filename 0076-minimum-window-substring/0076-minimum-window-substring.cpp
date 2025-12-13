786dcc7fa23f6d0b1f710f290409329ed9cdbaed
Mostafa khaled <mkostafa79@gmail.com>
2025-12-13 13:04:59 +0200

    Time: 33 ms (11.56%), Space: 12 MB (7.51%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,46 +1,72 @@
 class Solution {
 public:
-    bool isTheSame(int (&freq)[128], int (&freq2)[128]) {
-        for (int j = 0; j < 128; j++) {
-            if (freq2[j] < freq[j]) {
-                return false;
+    int map1[129];
+    int map2[129];
+    string minWindow(string s2, string s1) {
+
+        int sz1 = 0;
+        for (int i = 0; i < s1.size(); i++) {
+            if (map1[s1[i] - 'A'] == 0) {
+                sz1++;
             }
+            map1[s1[i] - 'A']++;
         }
-        return true;
-    }
-    string minWindow(string s, string t) {
 
-        int freq[128];
-        int freq2[128];
-
-        for (int i = 0; i < t.size(); i++) {
-            freq[t[i] - 'A']++;
-        }
-        int size = t.size() - 1;
         int start = 0;
-        int left = -1;
-        int right = s.size() + 110;
-        for (int i = 0; i < s.size(); i++) {
-            freq2[s[i] - 'A']++;
+        int sz2 = 0;
+        int len = INT_MAX;
+        string ans = s2;
+        int a1 = -1;
+        int a2 = -1;
+        for (int i = 0; i < s2.size(); i++) {
+            int cur = s2[i] - 'A';
+            if (map2[cur] == 0) {
+                sz2++;
+            }
+            map2[cur]++;
 
-            if (i >= size) {
-                while (i >= start && isTheSame(freq, freq2)) {
-                    if (right - left > i - start) {
-                        left = start;
-                        right = i;
+            while (i >= start && sz2 >= sz1) {
+                // string g = s2.substr(start, (i - start) + 1);
+                // sort(g.begin(), g.end());
+                // cout << g << endl;
+                bool valid = true;
+                for (int j = 0; j < 128; j++) {
+                    if (map2[j] < map1[j]) {
+                        valid = 0;
+
+                        break;
                     }
-                    freq2[s[start] - 'A']--;
-                    start++;
                 }
+                while (valid && i >= start && sz2 >= sz1) {
+                    int cur_ans = (i - start) + 1;
+                    if (a1 == -1 || cur_ans <= (a2 - a1) + 1) {
+                        len = cur_ans;
+
+                        a1 = start;
+                        a2 = i;
+                    }
+                    map2[s2[start] - 'A']--;
+                    if (map2[s2[start] - 'A'] == 0) {
+                        sz2--;
+                    }
+                    start++;
+                    for (int j = 0; j < 128; j++) {
+                        if (map2[j] < map1[j]) {
+
+                            valid = 0;
+
+                            break;
+                        }
+                    }
+                }
+
+                if (!valid)
+                    break;
             }
         }
-
-        if (left == -1)
+        if (len == INT_MAX || a1 == -1)
             return "";
-        string ans = "";
-        for (int i = left; i <= right; i++) {
-            ans += s[i];
-        }
-        return ans;
+
+        return s2.substr(a1, (a2 - a1) + 1);
     }
 };
