bc6f536c5407abc97f6a4b9cd52d77b4e55fac13
Mostafa khaled <mkostafa79@gmail.com>
2026-01-03 17:40:36 +0200

    Time: 0 ms (100%), Space: 21.9 MB (37.26%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -2,19 +2,15 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
-
-        // code to get sum from 0 to n
-        // code to get sum form this array nums
-
         int sum1 = 0;
         for (int i = 0; i < nums.size(); i++) {
-            sum1 += nums[i];
+            sum1 ^= nums[i];
         }
         int sum2 = 0;
         for (int i = 0; i <= n; i++) {
-            sum2 += i;
+            sum2 ^= i;
         }
 
-        return sum2 - sum1 ; 
+        return sum2 ^ sum1;
     }
 };
