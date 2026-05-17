7f15b34ec9c3b8a96f0e1baad641da9782cc505f
Mostafa khaled <mkostafa79@gmail.com>
2026-05-18 02:54:00 +0300

    Time: 2431 ms (8.49%), Space: 494.1 MB (5.28%) - LeetHub

--- 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.cpp
+++ 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.cpp
@@ -1,16 +1,9 @@
 class Solution {
 public:
     int countMajoritySubarrays(vector<int>& nums, int target) {
-
-        //   unordered_map<int, int> freq;
-
-        int max_f = 0;
-        int max_e = 0;
         int ans = 0;
-        int start = 0;
         for (int i = 0; i < nums.size(); i++) {
             unordered_map<int, int> freq;
-
             for (int j = i; j < nums.size(); j++) {
                 int len = (j - i) + 1;
                 freq[nums[j]]++;
@@ -19,6 +12,7 @@ public:
                 }
             }
         }
+
         return ans;
     }
 };
