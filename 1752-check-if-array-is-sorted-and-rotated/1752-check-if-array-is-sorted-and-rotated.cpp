2d38c5d100421d293afe96e8fbf7776f0629e8a4
Mostafa khaled <mkostafa79@gmail.com>
2026-05-23 20:21:26 +0300

    Time: 0 ms (100%), Space: 11.6 MB (6.99%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -4,35 +4,38 @@ public:
         int end = 0;
         int mins = INT_MAX;
         int start = 0;
-        unordered_map<int , int> freq ; 
+        unordered_map<int, int> freq;
         for (int i = 0; i < nums.size(); i++) {
             if (nums[i] <= mins) {
-               
+
                 if (mins != nums[i])
                     start = i;
                 mins = nums[i];
                 end = i;
             }
-             freq[nums[i]]++;
+            freq[nums[i]]++;
         }
-        if(freq[mins] >= ((nums.size() +1)/2) )  return true ; 
+        if (freq[mins] > ((nums.size() + 1) / 2))
+            return true;
         bool check1 = true;
-          bool check2 = true;
+        bool check2 = true;
         for (int i = 0; i < nums.size() - 1; i++) {
             int cur = (i + end) % nums.size();
             int next = (i + end + 1) % nums.size();
-            if (nums[cur] > nums[next] ) { check1 = false;
-              break ;
+            if (nums[cur] > nums[next]) {
+                check1 = false;
+                break;
             }
         }
 
-         for (int i = 0; i < nums.size() - 1; i++) {
+        for (int i = 0; i < nums.size() - 1; i++) {
             int cur = (i + start) % nums.size();
             int next = (i + start + 1) % nums.size();
-            if (nums[cur] > nums[next] ) { check2 = false;
-              break ;
+            if (nums[cur] > nums[next]) {
+                check2 = false;
+                break;
             }
         }
-       return check2 | check1 ;
+        return check2 | check1;
     }
 };
