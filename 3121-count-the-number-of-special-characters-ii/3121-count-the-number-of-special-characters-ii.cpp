cf5f0407c7a847075ac5d465b234d2d8a1875e09
Mostafa khaled <mkostafa79@gmail.com>
2026-05-27 11:44:20 +0300

    Time: 67 ms (73.03%), Space: 24.3 MB (68.82%) - LeetHub

--- 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.cpp
+++ 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
     int numberOfSpecialChars(string word) {
-        int freq1[26] = {};
-        int freq2[26] = {};
-        int skip[26] = {};
-        int take[26] = {};
+        int freq1[26] = {0};
         int ans = 0;
         for (int i = 0; i < word.size(); i++) {
             char cur = word[i];
+            //    cout << cur - 'a' << " ";
             if (cur - 'a' >= 0 && cur - 'a' < 26) {
-                freq1[cur - 'a']++;
-                if (freq2[cur - 'a'] == -1) {
-                    skip[cur - 'a'] = 1;
-                }
-
+                if (freq1[cur - 'a'] == -1)
+                    continue;
+                if (freq1[cur - 'a'] == 2) {
+                    freq1[cur - 'a'] = -1;
+                } else
+                    freq1[cur - 'a'] = 1;
             }
 
             else if (cur - 'A' >= 0 && cur - 'A' < 26) {
-                if (freq1[cur - 'A'] > 0 && freq2[cur - 'A'] != -1) {
-                    take[cur - 'A'] = 1;
+                if (freq1[cur - 'A'] == 2)
+                    continue;
+                if (freq1[cur - 'A'] == 1) {
+                    freq1[cur - 'A'] = 2;
+                } else {
+                    freq1[cur - 'A'] = -1;
                 }
-
-                freq2[cur - 'A'] = -1;
             }
         }
 
         for (int i = 0; i < 26; i++) {
-            if (take[i] == 1 && skip[i] == 0)
+            if (freq1[i] == 2)
                 ans++;
         }
 
