c0b2a79fbc0c0e3af2c0c3d251080cb2f2a6cf43
Mostafa khaled <mkostafa79@gmail.com>
2026-06-02 06:32:28 +0300

    Time: 360 ms (13.18%), Space: 73.5 MB (79.85%) - LeetHub

--- 1178-number-of-valid-words-for-each-puzzle/1178-number-of-valid-words-for-each-puzzle.cpp
+++ 1178-number-of-valid-words-for-each-puzzle/1178-number-of-valid-words-for-each-puzzle.cpp
@@ -4,14 +4,14 @@ public:
                                     vector<string>& puzzles) {
         int p = puzzles.size();
         vector<int> ans(p);
-        vector<int> masks(words.size());
+        unordered_map<int, int> map;
         for (int i = 0; i < words.size(); i++) {
             int mask = 0;
             for (char w : words[i]) {
                 mask |= (1 << (w - 'a'));
             }
 
-            masks[i] = mask;
+            map[mask]++;
         }
 
         for (int i = 0; i < p; i++) {
@@ -21,9 +21,9 @@ public:
                 init |= (1 << (w - 'a'));
             }
             int cur = 0;
-            for (int e : masks) {
-                if ((e & init) == e && (e & firstBit) != 0) {
-                    cur++;
+            for (auto e : map) {
+                if ((e.first & init) == e.first && (e.first & firstBit) != 0) {
+                    cur += e.second;
                 }
             }
 
