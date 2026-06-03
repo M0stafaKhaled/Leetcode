16a77ec71f2cc6859e1bcc947ef30920df41906e
Mostafa khaled <mkostafa79@gmail.com>
2026-06-03 06:14:14 +0300

    Time: 102 ms (5.07%), Space: 19.1 MB (10.14%) - LeetHub

---
+++ 1764-form-array-by-concatenating-subarrays-of-another-array/1764-form-array-by-concatenating-subarrays-of-another-array.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    bool canChoose(vector<vector<int>>& groups, vector<int>& nums) {
+
+        vector<vector<int>> lps(groups.size());
+
+        for (int i = 0; i < groups.size(); i++) {
+
+            vector<int> cur(groups[i].size(), 0);
+            int start = 0;
+            for (int j = 1; j < groups[i].size(); j++) {
+                if (groups[i][j] == groups[i][start]) {
+                    cur[j] = (start) + 1;
+                    start++;
+                } else {
+
+                    while (start) {
+                        if (groups[i][j] == groups[i][start]) {
+                            cur[j] = (start) + 1;
+                            start++;
+                            break;
+                        }
+                        start--;
+                    }
+                }
+            }
+
+            lps[i] = cur;
+        }
+        int j = 0;
+        for (int i = 0; i < lps.size(); i++) {
+            int a = 0;
+            int b = 0;
+
+            for (; j < nums.size(); j++) {
+                if (a == groups[i].size()) {
+                    break;
+                }
+                cout << a << " " << j << endl;
+                if (nums[j] == groups[i][a]) {
+                    a++;
+                    cout << "test" << endl;
+                } else {
+                    if (a == 0)
+                        continue;
+
+                    while (a) {
+                        a = lps[i][a - 1];
+                        if (nums[j] == groups[i][a]) {
+                            a++;
+                            break ; 
+                        }
+                       
+                    }
+                }
+            }
+
+            if (a != groups[i].size()) {
+                cout << i << endl;
+                return 0;
+            }
+        }
+
+        return true;
+    }
+};
